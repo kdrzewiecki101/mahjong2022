303044536fc387d055e3bb89967ef7af24a922ae
kdrzewiecki101 <k.drzewiecki101@gmail.com>
2022-05-29 23:59:57 +0200

    raycaster click

--- static/js/Game.js
+++ static/js/Game.js
@@ -16,25 +16,35 @@ class Game {
         this.secondFloor = [];
         this.thirdFloor = [];
         this.fourthFloor = [];
-        this.pieceH = 2.2
-        // scena 3D
+        this.pieceH = 2.2;
+        this.boards = new THREE.Group();
+        // Scena3D
         this.scene = new THREE.Scene();
-        this.scene.background = "../gfx/forest.jpg";
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.axes = new THREE.AxesHelper(1000)
+        this.scene.add(this.axes);
+        // Renderer
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setClearColor(0x483d8b);
         this.renderer.setClearAlpha(0);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.getElementById("root").append(this.renderer.domElement);
+        // Camera
         this.camera.position.set(100, 100, 0);
-        // console.log(this.camera);
         this.camera.lookAt(this.scene.position);
-        this.axes = new THREE.AxesHelper(1000)
-        this.scene.add(this.axes);
+        // OrbitControls
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.enablePan = false;
         this.controls.maxDistance = 80;
         this.controls.update();
+        // Raycaster
+        this.raycaster = new THREE.Raycaster();
+        // Pointer (wskaźnik)
+        this.pointer = new THREE.Vector2();
+        // Listeners
+        //window.addEventListener('pointermove', this.onPointerMove);
+        window.addEventListener('click', this.onPointerClick);
+        // Functions
         this.createBoard();
         this.createFloor(this.board.zeroFloor, this.pieceH * 1);
         this.createFloor(this.board.firstFloor, this.pieceH * 2);
@@ -42,6 +52,7 @@ class Game {
         this.createFloor(this.board.thirdFloor, this.pieceH * 4);
         this.createTopPiece(this.pieceH * 5);
         this.createSidewaysPieces(this.pieceH * 1);
+        this.scene.add(this.boards);
         this.render();
     }
 
@@ -58,23 +69,6 @@ class Game {
         }
 
 
-    }
-
-
-    createBoard = () => {
-        const geometry = new THREE.BoxGeometry(75, 3, 75);
-        const material = new THREE.MeshBasicMaterial({
-            side: THREE.DoubleSide,
-            // map: new THREE.TextureLoader().load('../gfx/diamond.png'),
-            wireframe: false,
-            transparent: true,
-            opacity: 1,
-            // color: 0x000000
-            color: 0xd2d2d2
-        });
-        const cube = new THREE.Mesh(geometry, material);
-
-        this.scene.add(cube);
     }
     createFloor = (floor, pieceH) => {
         for (let i = -4; i < Number(floor.length - 4); i++) {
@@ -131,11 +125,65 @@ class Game {
         rightPiece2.position.z = (-8 * zmiennaZ + 0.5 * zmiennaZ);
         this.scene.add(rightPiece2);
     }
+    createBoard = () => {
+        const geometry = new THREE.BoxGeometry(75, 3, 75);
+        const material = new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            // map: new THREE.TextureLoader().load('../gfx/diamond.png'),
+            wireframe: false,
+            transparent: true,
+            opacity: 1,
+            // color: 0x000000
+            color: 0xd2d2d2
+        });
+        const cube = new THREE.Mesh(geometry, material);
+        this.boards.add(cube);
+    }
+
+    onPointerMove = (event) => {
+        this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        this.pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+        this.hoverPieces();
+    }
+    onPointerClick = (event) => {
+        this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        this.pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+        this.clickPieces();
+    }
+    hoverPieces = () => {
+        this.raycaster.setFromCamera(this.pointer, this.camera);
+        const intersects = this.raycaster.intersectObjects(this.scene.children);
+        console.log(intersects.length);
+        for (let i = 0; i < intersects.length; i++) {
+            if (intersects.length > 0 && intersects[0].object.name == "clickable") {
+                intersects[0].object.material = new THREE.MeshBasicMaterial({
+                    color: 0xff0000, transparent: false,
+                    opacity: 1,
+                });
+                //console.log(intersects[0].object);
+            }
+        }
+    }
+    clickPieces = () => {
+        this.raycaster.setFromCamera(this.pointer, this.camera);
+        const intersects = this.raycaster.intersectObjects(this.scene.children);
+        console.log(intersects.length)
+        for (let i = 0; i < intersects.length; i++) {
+            console.log(intersects[0].object.name);
+            if (intersects.length > 0 && intersects[0].object.name == "clickable") {
+                intersects[0].object.material = new THREE.MeshBasicMaterial({
+                    color: 0x00ff00, transparent: false,
+                    opacity: 1,
+                });
+                //console.log(intersects[0].object);
+            }
+        }
+    }
     render = () => {
-        requestAnimationFrame(this.render);
-        // console.log("render leci")
+        //console.log("render leci")        
         this.controls.update();
         this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.render);
     }
 }
 
