a4ed3397af14b91c167cc3d5d90f4942d25b66b1
kdrzewiecki101 <k.drzewiecki101@gmail.com>
2022-06-13 21:49:35 +0200

    update server.js v2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 let users = [];
 app.use(express.static(path.join(__dirname, './static')));
